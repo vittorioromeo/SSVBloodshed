b4c4b293445d6af04553294255473ba0d20c8c9d
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-10-13 15:57:11 +0200

    Removed unused methods and fixed huge bug with static

--- include/SSVBloodshed/CESystem/Common.h
+++ include/SSVBloodshed/CESystem/Common.h
@@ -38,45 +38,37 @@ namespace ssvces
 
 	namespace Internal
 	{
-		// Last used bit index
-		static unsigned int lastTypeIdBitIdx{0};
+		inline std::size_t getNextTypeId() { static std::size_t lastTypeIdBitIdx{0}; return lastTypeIdBitIdx++; }
 
 		template<typename T> struct TypeIdStorage
 		{
 			// TypeIdStorage statically stores the TypeId and bit index of a Component type
-
 			static_assert(std::is_base_of<Component, T>::value, "TypeIdStorage only works with types that derive from Component");
-
-			static const TypeId typeId;
 			static const std::size_t bitIdx;
 		};
-		template<typename T> const TypeId TypeIdStorage<T>::typeId{typeid(T).hash_code()};
-		template<typename T> const std::size_t TypeIdStorage<T>::bitIdx{lastTypeIdBitIdx++};
+		template<typename T> const std::size_t TypeIdStorage<T>::bitIdx{getNextTypeId()};
 
 		// These functions use variadic template recursion to "build" a bitset for a set of Component types
-		template<typename T> inline static void buildBitsetHelper(TypeIdsBitset& mBitset) noexcept { mBitset[Internal::TypeIdStorage<T>::bitIdx] = true; }
-		template<typename T1, typename T2, typename... TArgs> inline static void buildBitsetHelper(TypeIdsBitset& mBitset) noexcept { buildBitsetHelper<T1>(mBitset); buildBitsetHelper<T2, TArgs...>(mBitset); }
-		template<typename... TArgs> inline static TypeIdsBitset getBuildBitset() noexcept { TypeIdsBitset result; buildBitsetHelper<TArgs...>(result); return result; }
+		template<typename T> inline void buildBitsetHelper(TypeIdsBitset& mBitset) noexcept { mBitset[Internal::TypeIdStorage<T>::bitIdx] = true; }
+		template<typename T1, typename T2, typename... TArgs> inline void buildBitsetHelper(TypeIdsBitset& mBitset) noexcept { buildBitsetHelper<T1>(mBitset); buildBitsetHelper<T2, TArgs...>(mBitset); }
+		template<typename... TArgs> inline TypeIdsBitset getBuildBitset() noexcept { TypeIdsBitset result; buildBitsetHelper<TArgs...>(result); return result; }
 
 		template<typename... TArgs> struct TypeIdsBitsetStorage{ static const TypeIdsBitset bitset; };
 		template<typename... TArgs> const TypeIdsBitset TypeIdsBitsetStorage<TArgs...>::bitset{getBuildBitset<TArgs...>()};
 		template<> const TypeIdsBitset TypeIdsBitsetStorage<>::bitset{};
 	}
 
-	// Shortcut to get the static TypeId of a Component type from TypeIdStorage
-	template<typename T> inline constexpr const TypeId& getTypeId() noexcept { return Internal::TypeIdStorage<T>::typeId; }
-
 	// Shortcut to get the bit index of a Component type
-	template<typename T> inline constexpr static const std::size_t& getTypeIdBitIdx() noexcept { return Internal::TypeIdStorage<T>::bitIdx; }
+	template<typename T> inline constexpr const std::size_t& getTypeIdBitIdx() noexcept { return Internal::TypeIdStorage<T>::bitIdx; }
 
 	// Shortcut to get the static Bitset of a pack of Component types
-	template<typename... TArgs> inline constexpr static const TypeIdsBitset& getTypeIdsBitsetStorage() noexcept { return Internal::TypeIdsBitsetStorage<TArgs...>::bitset; }
+	template<typename... TArgs> inline constexpr const TypeIdsBitset& getTypeIdsBitsetStorage() noexcept { return Internal::TypeIdsBitsetStorage<TArgs...>::bitset; }
 
 	// Returns whether the first bitset contains all the value of the second one
-	inline static bool containsAll(const TypeIdsBitset& mA, const TypeIdsBitset& mB) noexcept { return (mA & mB) == mB; }
+	inline bool containsAll(const TypeIdsBitset& mA, const TypeIdsBitset& mB) noexcept { return (mA & mB) == mB; }
 
 	class SystemBase;
-	inline static bool matchesSystem(const TypeIdsBitset& mTypeIds, const SystemBase& mSystem) noexcept;
+	inline bool matchesSystem(const TypeIdsBitset& mTypeIds, const SystemBase& mSystem) noexcept;
 }
 
 #endif
