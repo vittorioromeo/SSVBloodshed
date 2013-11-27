8a80bc5108cdb306cc03d9dd555c7c3c1f17ebb9
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-11-27 14:13:22 +0100

    noexcept

--- include/SSVBloodshed/GUI/AABBShape.hpp
+++ include/SSVBloodshed/GUI/AABBShape.hpp
@@ -18,37 +18,37 @@ namespace ob
 			AABBShape() = default;
 			AABBShape(const Vec2f& mPosition, const Vec2f& mHalfSize) { AABBShape::setSize(mHalfSize * 2.f); AABBShape::setPosition(mPosition); }
 
-			inline void setPosition(const Vec2f& mPosition)		{ sf::RectangleShape::setPosition(mPosition); }
-			inline void setPosition(float mX, float mY)			{ AABBShape::setPosition(Vec2f{mX, mY}); }
-			inline void setSize(const Vec2f& mSize)				{ sf::RectangleShape::setSize(mSize); sf::RectangleShape::setOrigin(getHalfSize()); }
-			inline void setSize(float mX, float mY)				{ AABBShape::setSize(Vec2f{mX, mY}); }
-			inline void setWidth(float mWidth) noexcept			{ AABBShape::setSize(mWidth, getHeight()); }
-			inline void setHeight(float mHeight) noexcept		{ AABBShape::setSize(getWidth(), mHeight); }
-			inline void setX(float mX) noexcept					{ AABBShape::setPosition(mX, getY()); }
-			inline void setY(float mY) noexcept					{ AABBShape::setPosition(getX(), mY); }
+			inline void setPosition(const Vec2f& mPosition) noexcept	{ sf::RectangleShape::setPosition(mPosition); }
+			inline void setPosition(float mX, float mY) noexcept		{ AABBShape::setPosition(Vec2f{mX, mY}); }
+			inline void setSize(const Vec2f& mSize) noexcept			{ sf::RectangleShape::setSize(mSize); sf::RectangleShape::setOrigin(getHalfSize()); }
+			inline void setSize(float mX, float mY) noexcept			{ AABBShape::setSize(Vec2f{mX, mY}); }
+			inline void setWidth(float mWidth) noexcept					{ AABBShape::setSize(mWidth, getHeight()); }
+			inline void setHeight(float mHeight) noexcept				{ AABBShape::setSize(getWidth(), mHeight); }
+			inline void setX(float mX) noexcept							{ AABBShape::setPosition(mX, getY()); }
+			inline void setY(float mY) noexcept							{ AABBShape::setPosition(getX(), mY); }
 
-			inline float getX() const noexcept					{ return sf::RectangleShape::getPosition().x; }
-			inline float getY() const noexcept					{ return sf::RectangleShape::getPosition().y; }
-			inline float getLeft() const noexcept				{ return sf::RectangleShape::getPosition().x - getHalfSize().x; }
-			inline float getRight() const noexcept				{ return sf::RectangleShape::getPosition().x + getHalfSize().x; }
-			inline float getTop() const noexcept				{ return sf::RectangleShape::getPosition().y - getHalfSize().y; }
-			inline float getBottom() const noexcept				{ return sf::RectangleShape::getPosition().y + getHalfSize().y; }
-			inline Vec2f getHalfSize() const noexcept			{ return sf::RectangleShape::getSize() / 2.f; }
-			inline float getHalfWidth() const noexcept			{ return getHalfSize().x; }
-			inline float getHalfHeight() const noexcept			{ return getHalfSize().y; }
-			inline float getWidth() const noexcept				{ return sf::RectangleShape::getSize().x; }
-			inline float getHeight() const noexcept				{ return sf::RectangleShape::getSize().y; }
+			inline float getX() const noexcept			{ return sf::RectangleShape::getPosition().x; }
+			inline float getY() const noexcept			{ return sf::RectangleShape::getPosition().y; }
+			inline float getLeft() const noexcept		{ return sf::RectangleShape::getPosition().x - getHalfSize().x; }
+			inline float getRight() const noexcept		{ return sf::RectangleShape::getPosition().x + getHalfSize().x; }
+			inline float getTop() const noexcept		{ return sf::RectangleShape::getPosition().y - getHalfSize().y; }
+			inline float getBottom() const noexcept		{ return sf::RectangleShape::getPosition().y + getHalfSize().y; }
+			inline Vec2f getHalfSize() const noexcept	{ return sf::RectangleShape::getSize() / 2.f; }
+			inline float getHalfWidth() const noexcept	{ return getHalfSize().x; }
+			inline float getHalfHeight() const noexcept	{ return getHalfSize().y; }
+			inline float getWidth() const noexcept		{ return sf::RectangleShape::getSize().x; }
+			inline float getHeight() const noexcept		{ return sf::RectangleShape::getSize().y; }
 
-			inline void resizeFromLeft(float mWidth) 			{ setX(getX() - (mWidth - getWidth()) / 2.f); setWidth(mWidth); }
-			inline void resizeFromRight(float mWidth) 			{ setX(getX() + (mWidth - getWidth()) / 2.f); setWidth(mWidth); }
-			inline void resizeFromTop(float mHeight) 			{ setY(getY() - (mHeight - getHeight()) / 2.f); setHeight(mHeight); }
-			inline void resizeFromBottom(float mHeight) 		{ setY(getY() + (mHeight - getHeight()) / 2.f); setHeight(mHeight); }
-			inline void resizeFromNW(const Vec2f& mSize) 		{ resizeFrom(Vec2f{-1.f, -1.f}, mSize); }
-			inline void resizeFromNE(const Vec2f& mSize) 		{ resizeFrom(Vec2f{1.f, -1.f}, mSize); }
-			inline void resizeFromSW(const Vec2f& mSize) 		{ resizeFrom(Vec2f{-1.f, 1.f}, mSize); }
-			inline void resizeFromSE(const Vec2f& mSize) 		{ resizeFrom(Vec2f{1.f, 1.f}, mSize); }
+			inline void resizeFromLeft(float mWidth) noexcept		{ setX(getX() - (mWidth - getWidth()) / 2.f); setWidth(mWidth); }
+			inline void resizeFromRight(float mWidth) noexcept		{ setX(getX() + (mWidth - getWidth()) / 2.f); setWidth(mWidth); }
+			inline void resizeFromTop(float mHeight) noexcept		{ setY(getY() - (mHeight - getHeight()) / 2.f); setHeight(mHeight); }
+			inline void resizeFromBottom(float mHeight) noexcept	{ setY(getY() + (mHeight - getHeight()) / 2.f); setHeight(mHeight); }
+			inline void resizeFromNW(const Vec2f& mSize) noexcept	{ resizeFrom(Vec2f{-1.f, -1.f}, mSize); }
+			inline void resizeFromNE(const Vec2f& mSize) noexcept	{ resizeFrom(Vec2f{1.f, -1.f}, mSize); }
+			inline void resizeFromSW(const Vec2f& mSize) noexcept	{ resizeFrom(Vec2f{-1.f, 1.f}, mSize); }
+			inline void resizeFromSE(const Vec2f& mSize) noexcept	{ resizeFrom(Vec2f{1.f, 1.f}, mSize); }
 
-			inline void resizeFrom(const Vec2f& mOrigin, const Vec2f& mSize)
+			inline void resizeFrom(const Vec2f& mOrigin, const Vec2f& mSize) noexcept
 			{
 				assert(mOrigin.x >= 0.f && mOrigin.x <= 1.f);
 				assert(mOrigin.y >= 0.f && mOrigin.y <= 1.f);
