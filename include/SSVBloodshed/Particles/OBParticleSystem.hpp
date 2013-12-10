e65d87a56b682dd2b9222bf32e11259840e6f224
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-12-10 15:21:29 +0100

    Particle optimization/overhaul/testing

--- include/SSVBloodshed/Particles/OBParticleSystem.hpp
+++ include/SSVBloodshed/Particles/OBParticleSystem.hpp
@@ -18,7 +18,7 @@ namespace ob
 			std::vector<OBParticle> particles;
 
 		public:
-			inline OBParticleSystem() { vertices.reserve(OBConfig::getParticleMax() * 4); particles.reserve(OBConfig::getParticleMax()); }
+			inline OBParticleSystem() { vertices.resize(OBConfig::getParticleMax() * 4); particles.reserve(OBConfig::getParticleMax()); }
 			template<typename... TArgs> inline void emplace(TArgs&&... mArgs) { particles.emplace_back(std::forward<TArgs>(mArgs)...); }
 			inline void update(FT mFT)
 			{
@@ -26,19 +26,27 @@ namespace ob
 				if(particles.size() > OBConfig::getParticleMax()) particles.erase(std::begin(particles) + OBConfig::getParticleMax(), std::end(particles));
 
 				ssvu::eraseRemoveIf(particles, [](const OBParticle& mParticle){ return mParticle.getLife() <= 0; });
-				vertices.clear();
-				for(auto& p : particles)
+				for(auto i(0u); i < particles.size(); ++i)
 				{
-					p.update(mFT);
-					vertices.emplace_back(Vec2f{p.getPosition().x - p.getSize(), p.getPosition().y - p.getSize()}, p.getColor());
-					vertices.emplace_back(Vec2f{p.getPosition().x + p.getSize(), p.getPosition().y - p.getSize()}, p.getColor());
-					vertices.emplace_back(Vec2f{p.getPosition().x + p.getSize(), p.getPosition().y + p.getSize()}, p.getColor());
-					vertices.emplace_back(Vec2f{p.getPosition().x - p.getSize(), p.getPosition().y + p.getSize()}, p.getColor());
+					auto& p(particles[i]); p.update(mFT);
+					const auto& vIdx(i * 4);
+
+					constexpr float fuzzyness{1.f};
+
+					// TODO: add fuzzyness to particle type
+					// TODO: store l/r/t/b in particle
+					vertices[vIdx + 0].position = Vec2f{p.getPosition().x - p.getSize() + ssvu::getRndR(-fuzzyness, fuzzyness), p.getPosition().y - p.getSize() + ssvu::getRndR(-fuzzyness, fuzzyness)};
+					vertices[vIdx + 1].position = Vec2f{p.getPosition().x - p.getSize() + ssvu::getRndR(-fuzzyness, fuzzyness), p.getPosition().y + p.getSize() + ssvu::getRndR(-fuzzyness, fuzzyness)};
+					vertices[vIdx + 2].position = Vec2f{p.getPosition().x + p.getSize() + ssvu::getRndR(-fuzzyness, fuzzyness), p.getPosition().y - p.getSize() + ssvu::getRndR(-fuzzyness, fuzzyness)};
+					vertices[vIdx + 3].position = Vec2f{p.getPosition().x + p.getSize() + ssvu::getRndR(-fuzzyness, fuzzyness), p.getPosition().y + p.getSize() + ssvu::getRndR(-fuzzyness, fuzzyness)};
+					vertices[vIdx + 0].color = p.getColor();
+					vertices[vIdx + 1].color = p.getColor();
+					vertices[vIdx + 2].color = p.getColor();
+					vertices[vIdx + 3].color = p.getColor();
 				}
 			}
-			inline void draw(sf::RenderTarget& mRenderTarget, sf::RenderStates mRenderStates) const override { mRenderTarget.draw(vertices, mRenderStates); }
-			inline void clear() { vertices.clear(); particles.clear(); }
-
+			inline void draw(sf::RenderTarget& mRenderTarget, sf::RenderStates mRenderStates) const override { mRenderTarget.draw(&vertices[0], particles.size() * 4, sf::PrimitiveType::Quads, mRenderStates); }
+			inline void clear() { particles.clear(); }
 	};
 }
 
