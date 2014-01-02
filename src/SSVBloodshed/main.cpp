95ed7660954a5fccc6f4f8320244baaf8c44c3b0
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-01-02 22:22:01 +0100

    copyright

--- src/SSVBloodshed/main.cpp
+++ src/SSVBloodshed/main.cpp
@@ -221,7 +221,7 @@ int main()
 
 #ifdef BLOODSHED
 
-// Copyright (c) 2013 Vittorio Romeo
+// Copyright (c) 2013-2014 Vittorio Romeo
 // License: Academic Free License ("AFL") v. 3.0
 // AFL License page: http://opensource.org/licenses/AFL-3.0
 
