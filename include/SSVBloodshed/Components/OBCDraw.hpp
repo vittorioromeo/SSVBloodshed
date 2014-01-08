28e79a84a0a9691a2560a902cde00c7a80a02897
Vittorio Romeo <vittorio.romeo@outlook.com>
2014-01-08 16:51:04 +0100

    Removed push (now uses emplacement)

--- include/SSVBloodshed/Components/OBCDraw.hpp
+++ include/SSVBloodshed/Components/OBCDraw.hpp
@@ -44,7 +44,6 @@ namespace ob
 			}
 			inline void draw() override { for(const auto& s : sprites) game.render(s, blendMode); }
 
-			inline void pushSprite(const sf::Sprite& mSprite)						{ sprites.push_back(mSprite); offsets.emplace_back(); }
 			template<typename... TArgs> inline void emplaceSprite(TArgs&&... mArgs)	{ sprites.emplace_back(std::forward<TArgs>(mArgs)...); offsets.emplace_back(); }
 
 			inline void rotate(float mDeg) noexcept						{ for(auto& s : sprites) s.rotate(mDeg); }
